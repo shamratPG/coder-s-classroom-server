79d05cb4a4c8b461396d309618db8c5ca272bd7f
shamratPG <shamratpg@gmail.com>
2022-10-25 13:52:14 +0600

    added Blogs info

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-const courseItems = require('./data/courses.json')
+const courseItems = require('./data/courses.json');
+const blogs = require('./data/blogs.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -13,6 +14,10 @@ app.get('/', (req, res) => {
 app.get('/courses', (req, res) => {
     res.send(courseItems);
 });
+
+app.get('/blogs', (req, res) => {
+    res.send(blogs);
+})
 
 app.listen(port, () => {
     console.log('Server is running')
