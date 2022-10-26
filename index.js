d33fbc05eea9c05bdb562ab9cb3891cc95c29df5
shamratPG <shamratpg@gmail.com>
2022-10-26 13:23:00 +0600

    added functions to find specific item

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000;
+// const port = 5000;
 
 app.use(cors())
 
@@ -17,9 +18,23 @@ app.get('/courses', (req, res) => {
     res.send(courseItems);
 });
 
+app.get('/courses/:courseId', (req, res) => {
+    const id = req.params.courseId;
+    console.log(id);
+    const selectedCourse = courseItems.find(course => course.id == id)
+    res.send(selectedCourse);
+});
+
 app.get('/blogs', (req, res) => {
     res.send(blogs);
 })
+
+app.get('/blogs/:blogId', (req, res) => {
+    const id = req.params.blogId;
+    console.log(id);
+    const selectedBlog = blogs.find(blog => blog.id == id)
+    res.send(selectedBlog);
+});
 
 app.listen(port, () => {
     console.log('Server is running')
